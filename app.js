b40d0b0ede936ed19c89b9874dcf036564aaf1fe
xzdil <82775283+xzdil@users.noreply.github.com>
2023-11-15 21:22:45 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 9001;
 
 app.use(express.static('public'));
 //app.use(express.urlencoded({ extended: true }));
